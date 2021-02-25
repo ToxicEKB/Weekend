e4cc5a98d4321b908a688eb0345c1c5cce5b777f
Elena Ditiateva <elena2091@bk.ru>
2021-02-25 22:32:20 +0300

    Fix components. Create Tags

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import Footer from './footer/Footer.js';
-// This one import will make Tailwind’s utility classes available to the entire app,
-// no need to import it again elsewhere.
 import "./tailwind.output.css";
+import Tags from './components/Tags/Tags';
+import Footer from './components/footer/Footer.js';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/*example usage Tailwind below*/}
-        <p className="border-solid border-4 border-gray-600">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Tags />
       <Footer />
     </div>
   );
