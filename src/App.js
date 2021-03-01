c91952685123c3a542c82706123281afce22b84a
Elena Ditiateva <elena2091@bk.ru>
2021-03-01 23:19:37 +0300

    added useEffect in PopUp

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+import React, { useState, useEffect } from "react";
 import "./tailwind.output.css";
-import Tags from './components/Tags/Tags';
-import PopUp from './components/PopUp';
-import Footer from './components/footer/Footer.js';
+import Tags from "./components/Tags";
+import PopUp from "./components/PopUp";
+import Footer from "./components/footer/Footer.js";
 
 
 function App() {
+  const [visiblePopUp, setvisiblePopUp] = useState(true);
+
+  useEffect(() => {
+    setvisiblePopUp(visiblePopUp);
+  }, []);
+
+  const togglePopUp = () => {
+    setvisiblePopUp(!visiblePopUp);
+  };
+
   return (
     <div className="App">
+      {visiblePopUp ? <PopUp updateData={togglePopUp} /> : null}
       <Tags />
-      <PopUp />
       <Footer />
     </div>
   );
