30810f5e4048fb03be2caacf5a727e2c2fe5d28f
Konfeta <konfeta333@gmail.com>
2021-03-02 08:49:33 +0300

    [fixed] About Component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,8 +4,8 @@ const About = () => {
   return (
     <div className="w-96 md:w-full xl:w-full mx-auto flex flex-col text-center mb-7">
       <h1 className="text-green-600 w-full text-xl font-bold px-5 mb-7">Weekend c детьми</h1>
-      <span className="w-full px-5">
-        это уникальный сервис по поиску различных вариантов досуга с детьми, который приятно удивит
+      <span className="w-full px-5 text-base">
+        Это уникальный сервис по поиску различных вариантов досуга с детьми, который приятно удивит
         Вас своей простотой и информативностью Чтобы поиск был легче, выберите интересующую категорию
       </span>
     </div>
