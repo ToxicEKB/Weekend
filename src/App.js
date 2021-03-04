446d4a910e21fb978bdf7f48817f625869108e03
EvgeniyaKa <56150618+EvgeniyaKa@users.noreply.github.com>
2021-03-04 22:42:14 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,21 +9,6 @@ function App() {
   // const routes = useRoutes();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/*example usage Tailwind below*/}
-        <p className="border-solid border-4 border-gray-600">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <Footer />
     </div>
   );
