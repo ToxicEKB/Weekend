102012b771a7f23325822ea748104de5d0db7940
Konfeta <konfeta333@gmail.com>
2021-03-10 17:34:19 +0300

    Fixed path to images, add MainImg

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,10 @@
 import React from "react";
+import MainImg from "./MainImg";
 
 const About = () => {
   return (
     <div className="w-96 md:w-full xl:w-full mx-auto flex flex-col text-center mb-7">
+      <MainImg />
       <h1 className="text-green-600 w-full text-xl font-bold px-5 mb-7">Weekend c детьми</h1>
       <span className="w-full px-5 text-base mb-4">
         Это уникальный сервис по поиску различных вариантов досуга с детьми, который приятно удивит
