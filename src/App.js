be99554d31ebcf7ca6959f0feecfaf7bb0bd460f
Elena Ditiateva <elena2091@bk.ru>
2021-03-14 14:43:26 +0300

    добавлен схематичный header и z-index для модального окна

--- src/App.js
+++ src/App.js
@@ -6,11 +6,16 @@ import CategoryPage from "./pages/CategoryPage";
 import TagsPage from "./pages/TagsPage";
 import EventPage from "./pages/EventPage";
 import Footer from "./components/footer/Footer.js";
+import logo from "./images/logo.png";
 
 function App() {
   return (
     <React.StrictMode>
       <Router>
+        <div className="flex items-center border">
+          <img src={logo} alt="logo" className="justify-self-start" style={{ width: 159 }}/>
+          <h3 className="m-auto">Здесь будет хэдэр</h3>
+        </div>
         <Switch>
           <Route exact path="/" component={MainPage} />
           <Route path="/category" component={CategoryPage} />
