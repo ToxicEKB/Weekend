a645838205e9930e7c901e2f84a8d0d03902713f
Konfeta <konfeta333@gmail.com>
2021-03-15 10:55:06 +0300

    Fixed Category component

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -7,9 +7,9 @@ import { mockCategory } from "./mockCategory";
 const Category = () => {
   return (
     <div className="w-96 md:w-full xl:w-full mx-auto flex flex-col text-center mb-7">
-      <div className="bg-category rotate-180 transform sm:bg-Sea">
-        <div className="flex flex-wrap justify-center flex-col rotate-180 transform p-4">
-          <div className="flex justify-center items-center w-72 mb-5 mx-auto">
+      <div className="bg-category sm:bg-Sea bg-no-repeat bg-cover bg-center">
+        <div className="flex flex-wrap justify-center flex-col p-4">
+          <div className="flex justify-center items-center w-72 mb-5 mx-auto mt-4">
             <RectLeftWhite/>
             <h1 className="text-white w-full text-xl font-bold px-5 flex-shrink-0">Варианты
               отдыха</h1>
