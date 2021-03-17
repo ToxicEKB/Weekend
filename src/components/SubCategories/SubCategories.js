c335524b2e03c8a4ece43d78c711190b55562078
Konfeta <konfeta333@gmail.com>
2021-03-17 17:30:49 +0300

    Finished receiving Categories and SubCategories from backend

--- src/components/SubCategories/SubCategories.js
+++ src/components/SubCategories/SubCategories.js
@@ -1,27 +1,14 @@
 import SubCatItem from "./SubCatItem";
-import { subCatItems } from "./subCatItems";
-import { useQuery } from "react-query";
-import { getSubCategoriesFull, getSubCategoryById } from "../../helpers/requests";
 
-const SubCategories = ({ subCategoriesIds }) => {
-  console.log(subCategoriesIds);
-  const {
-    isLoading: subCategoriesLoading,
-    data: subCategories,
-  } = useQuery("subCategoriesFull", () => getSubCategoriesFull(subCategoriesIds));
-
-  if (subCategoriesLoading) return null;
-  console.log(subCategories);
+const SubCategories = ({ subCategories }) => {
 
   return (
     <div className="px-3 mb-5 max-w-sm mx-auto flex justify-between">
-      {/*{subCategories.map((item, idx) => {*/}
-      {/*  return (*/}
-      {/*    item.cat === cat && (*/}
-      {/*    <SubCatItem icon={item.icon} title={item.title} key={idx}/>*/}
-      {/*    )*/}
-      {/*  );*/}
-      {/*})}*/}
+      {subCategories?.map((item, idx) => {
+        return (
+          <SubCatItem item={item} key={idx}/>
+        );
+      })}
     </div>
   );
 };
